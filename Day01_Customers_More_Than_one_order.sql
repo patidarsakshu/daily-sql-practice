8995a26d5edb8ef1f31c4c22bf0214038c254ac4
sakshi patidar <103515914+patidarsakshu@users.noreply.github.com>
2026-02-12 21:44:39 +0530

    Fix SQL query to find customers with multiple orders
    Refactor SQL query to correctly count orders per customer and join with customers table.

--- Day01_Customers_More_Than_one_order.sql
+++ Day01_Customers_More_Than_one_order.sql
@@ -1,5 +1,57 @@
-## DAY01-> WAQ to find customers who ordered more than once
-select c_id count(order_id)AS total_orders
+-- Day 01
+-- Problem: Find customers who placed more than one order.
+
+-- Input Table: customers
+
+-- +-------------+---------------+
+-- | customer_id | customer_name |
+-- +-------------+---------------+
+-- | 101         | Asha          |
+-- | 102         | Ravi          |
+-- | 103         | Neha          |
+-- +-------------+---------------+
+
+-- Input Table: orders
+
+-- +----------+-------------+------------+
+-- | order_id | customer_id | order_date |
+-- +----------+-------------+------------+
+-- | 1        | 101         | 2024-01-01 |
+-- | 2        | 102         | 2024-01-02 |
+-- | 3        | 101         | 2024-01-03 |
+-- | 4        | 103         | 2024-01-04 |
+-- | 5        | 102         | 2024-01-05 |
+-- +----------+-------------+------------+
+
+
+-- Expected Output
+-- +-------------+---------------+--------------+
+-- | customer_id | customer_name | total_orders |
+-- +-------------+---------------+--------------+
+-- | 101         | Asha          | 2            |
+-- | 102         | Ravi          | 2            |
+-- +-------------+---------------+--------------+
+
+  
+select customer_id,count(order_id) AS total_orders
 from orders 
-group by c_id
+group by customer_id
 having count(order_id)>1;
+
+Output:
+-- +-------------+---------------+
+-- | customer_id | total_orders  |
+-- +-------------+---------------+
+-- | 101         |    2          |
+-- | 102         |    2          |
+-- +-------------+---------------+
+
+------------------OR----------------
+SELECT 
+    c.customer_id, 
+    c.customer_name, 
+    COUNT(o.order_id) AS total_orders
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.customer_name
+HAVING COUNT(o.order_id) > 1;
